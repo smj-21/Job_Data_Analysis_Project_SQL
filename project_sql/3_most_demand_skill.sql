5f8673f37693311d3f1eb00f8232a466c2657219
smj-21 <87064430+smj-21@users.noreply.github.com>
2026-03-18 08:24:41 +0530

    Add files via upload

---
+++ project_sql/3_most_demand_skill.sql
@@ -0,0 +1,10 @@
+SELECT
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY skills
+ORDER BY demand_count DESC
+LIMIT 20
