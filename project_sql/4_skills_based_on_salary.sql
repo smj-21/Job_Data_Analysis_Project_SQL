5edaaa6b9c4b60d1cbe4943ac3379dea78b2abf2
smj-21 <87064430+smj-21@users.noreply.github.com>
2026-04-12 13:11:44 +0530

    Update 4_skills_based_on_salary.sql

--- project_sql/4_skills_based_on_salary.sql
+++ project_sql/4_skills_based_on_salary.sql
@@ -1,14 +1,8 @@
-SELECT
-    skills,
-    ROUND(AVG(salary_year_avg),0) AS avg_salary
-FROM
-    job_postings_fact
-INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE
-    job_title_short = 'Data Analyst'
-    AND salary_year_avg IS NOT NULL
-    AND job_work_from_home = 'True'
-GROUP BY skills
-ORDER BY avg_salary DESC
-LIMIT 20
+select skills_dim.skills, round(avg(job_postings_fact.salary_year_avg),2) as avg_salary
+from job_postings_fact
+inner join skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+inner join skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE salary_year_avg is not null and job_title_short = 'Data Analyst'
+GROUP by skills_dim.skills
+order by avg_salary DESC
+limit 50
